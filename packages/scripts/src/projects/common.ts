3b9e1351bb1620a5e09ecc92a7a73b6017fd5812
enncy <877526278@qq.com>
2026-06-16 18:54:54 +0800

    perf(script): 添加随机答题设置仅支持超星章节测试的提示

--- packages/scripts/src/projects/common.ts
+++ packages/scripts/src/projects/common.ts
@@ -492,12 +492,12 @@ export const CommonProject = Project.create({
 				'randomWork-choice': {
 					defaultValue: false,
 					label: '(仅超星)随机选择',
-					attrs: { type: 'checkbox', title: '题库搜索不到答案时，随机选择任意一个选项' }
+					attrs: { type: 'checkbox', title: '题库搜索不到答案时，随机选择任意一个选项，仅支持超星章节测试' }
 				},
 				'randomWork-complete': {
 					defaultValue: false,
 					label: '(仅超星)随机填空',
-					attrs: { type: 'checkbox', title: '题库搜索不到答案时，随机填写以下任意一个文案' }
+					attrs: { type: 'checkbox', title: '题库搜索不到答案时，随机填写以下任意一个文案，仅支持超星章节测试' }
 				},
 				'randomWork-completeTexts-textarea': {
 					defaultValue: ['不会', '不知道', '不清楚', '不懂', '不会写'].join('\n'),
